46270c2a6bc6b21298b074047b501e5d00bb9b60
JBC363 <jameswebb512@gmail.com>
2018-01-30 18:21:43 +1100

    added gfx
    diesel_processor
    diesel
    combistion_mixture

---
+++ PowerPlusPlus_0.1.7/prototypes/item/diesel-generator.lua
@@ -0,0 +1,31 @@
+data.raw["item"]["steam-turbine"].order = "b[steam-power]-c[steam-turbine-1]"
+
+data:extend
+(
+{
+{
+    type = "item",
+    name = "diesel-generator",
+    icon = "__PowerPlusPlus__/graphics/icons/diesel-generator.png",
+    icon_size = 32,
+    flags = {"goes-to-quickbar"},
+    subgroup = "energy",
+    order = "b[steam-power]-b[steam-engine]",
+    place_result = "diesel-generator",
+    stack_size = 10
+},
+{
+    type = "item",
+    name = "diesel-processor",
+    icon = "__base__/graphics/icons/chemical-plant.png",
+    icon_size = 32,
+    flags = {"goes-to-quickbar"},
+    subgroup = "energy",
+    order = "b[steam-power]-b[steam-engine]",
+    place_result = "diesel-processor",
+    stack_size = 10
+}
+}
+)
+
+
