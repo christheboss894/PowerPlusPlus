b34303a245da9ab57332086cb8f347c7e556461d
christheboss894 <35890103+christheboss894@users.noreply.github.com>
2018-01-31 16:24:40 +1100

    Added Storage Tanks for both Combustion Mixture And Diesel Fuel

---
+++ PowerPlusPlus_0.2.3/data.lua
@@ -0,0 +1,21 @@
+if not power then power = {} end
+
+
+require("prototypes.entity.cooler")
+require("prototypes.item.cooler")
+require("prototypes.recipe.cooler")
+require("prototypes.technology.cooler")
+require("prototypes.technology.diesel-generation")
+require("prototypes.entity.diesel-generator")
+require("prototypes.recipe.diesel-generator")
+require("prototypes.item.diesel-generator")
+require("prototypes.fluid.diesel")
+require("prototypes.recipe.diesel-processing")
+require("prototypes.category")
+require("prototypes.entity.diesel-processor")
+require("prototypes.entity.combustion-mixture-generator")
+require("prototypes.technology.combustion-mixture")
+require("prototypes.fluid.combustion-mixture")
+require("prototypes.fluid.pressurized-water")
+require("prototypes.entity.water-turbine")
+require("prototypes.entity.diesel-tank")
